67c601753c7b242b4abb875c6f2646b0f3c040c6
MauricioLZ <mauriciolopeszugno_1@hotmail.com>
2023-11-04 14:53:25 +0000

    Tentative build

--- Program.cs
+++ Program.cs
@@ -1,30 +1,43 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Configuration;
+using System;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Env.azureConnectionString));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.Configure<Env>(builder.Configuration.GetSection("genealogica:Env"));
+
+        string appConfigConnectionString = builder.Configuration.GetConnectionString("AppConfig");
+        builder.Configuration.AddAzureAppConfiguration(appConfigConnectionString);
+        
+        string serverConnectionString = builder.Configuration.GetConnectionString("azureConnectionString");
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(serverConnectionString));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller}/{action=Index}/{id?}");
+
+        app.MapFallbackToFile("index.html");
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html");
-
-app.Run();
